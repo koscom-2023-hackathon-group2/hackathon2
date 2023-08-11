63f70240f4377ddaf3679ff1b2ace0a60cf3f1fc
Da Yeon Sung <sdy32@naver.com>
2023-08-11 19:50:50 +0900

    FEAT-12 계좌 개설 화면 stepper 구현

--- koscom_together_front/src/styles/NewAccountEmotion.js
+++ koscom_together_front/src/styles/NewAccountEmotion.js
@@ -10,6 +10,50 @@ export const NewAccountWrapper = styled.div`
   .title {
     font-size: 20px;
     font-weight: 700;
+    padding-bottom: 2rem;
+  }
+
+  .stepper-body {
+    padding: 0 0.5rem;
+  }
+`;
+
+export const NewAccountDescBox = styled.div`
+  width: calc(100% - 3rem);
+  padding: 1rem 1.5rem;
+  background-color: ${COLORS.gray400};
+  font-family: ${FONTS.NotoSansKR};
+
+  display: flex;
+  flex-direction: column;
+
+  .desc-title {
+    font-weight: 700;
     padding-bottom: 1rem;
+  }
+`;
+
+export const NewAccountContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: ${FONTS.NotoSansKR};
+  padding-top: 1.5rem;
+
+  .content-title {
+    font-weight: 700;
+    padding-bottom: 0.5rem;
+  }
+`;
+
+export const ConnectingAccountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: ${FONTS.NotoSansKR};
+  padding-top: 1.5rem;
+
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    padding-top: 0.5rem;
   }
 `;
