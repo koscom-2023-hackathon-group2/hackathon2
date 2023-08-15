8ce0763e5b8e8523f92210d86f5e165c72899dba
yunjiye <yjo3440@naver.com>
2023-08-15 19:45:04 +0900

    FEAT-28 login 수정

--- koscom_together_back/src/main/java/com/koscom/koscom_together_back/dto/MemberDto.java
+++ koscom_together_back/src/main/java/com/koscom/koscom_together_back/dto/MemberDto.java
@@ -1,9 +1,17 @@
 package com.koscom.koscom_together_back.dto;
 
+import com.koscom.koscom_together_back.domain.member.Member;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class MemberDto {
     private String id;
     private String password;
+
+    @Builder
+    MemberDto(Member member){
+        this.id = member.getId();
+        this.password = member.getPassword();
+    }
 }
